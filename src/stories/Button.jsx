f42ddca3e1ba125b859f01eb614257ccde0d4bb1
Brijesh Pant <pantbrijesh717@live.com>
2023-08-08 20:34:39 +0200

    refactor: :fire: cleanup

--- src/stories/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Button as MuiButton } from "@mui/material";
-
-/**
- * Primary UI Button component for user interaction
- */
-const Button = (props) => <MuiButton {...props} />;
-
-export default Button;
